f771ec3e4b8368486183071b2393c87958708484
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-04-26 14:57:58 +0900

    [Fix] 엔티티 컬럼 이름 수정

--- src/main/java/com/mumu/mumu/domain/Member.java
+++ src/main/java/com/mumu/mumu/domain/Member.java
@@ -14,15 +14,33 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long memberId;
 
+    @Column(name = "nickname")
     private String nickname;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "refresh_token")
     private String refreshToken;
+
+    @Column(name = "access_token")
     private String accessToken;
+
+    @Column(name = "age_range")
     private String ageRange;
+
+    @Column(name = "sex")
     private String sex;
+
+    @Column(name = "kakao_id")
     private String kakaoId;
+
+    @Column(name = "naver_id")
     private String naverId;
+
+    @Column(name = "google_id")
     private String googleId;
 }
