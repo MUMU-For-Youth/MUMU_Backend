b9f903fa6555dd36dc449c5beca4104098cf9b65
jinaen <fire_long346@naver.com>
2025-04-30 21:03:05 +0900

    [Feat] Multiple Filtering w/ ,

--- src/main/java/com/mumu/mumu/service/EduListService.java
+++ src/main/java/com/mumu/mumu/service/EduListService.java
@@ -6,7 +6,7 @@ import com.mumu.mumu.dto.EduDetailResponseDto;
 import java.util.List;
 
 public interface EduListService {
-    List<Edu> getEduList(String region, String field, String status);
+    List<Edu> getEduList(List<String> regions, List<String> fields, List<String> statuses);
 
     EduDetailResponseDto getEduById(Long eduId);
 }
