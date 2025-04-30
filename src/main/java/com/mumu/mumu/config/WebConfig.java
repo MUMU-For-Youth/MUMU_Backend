c11b91fc4a9be86450c453e9055d52817b8754a8
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-04-30 19:25:32 +0900

    [Bug] 프론트 개발 주소 허용

---
+++ src/main/java/com/mumu/mumu/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.mumu.mumu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 URL에 대해
+                .allowedOrigins("http://localhost:3000") // 프론트 주소 입력
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true); // 쿠키 등 인증정보 포함 허용
+    }
+}
